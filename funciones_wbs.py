42931322a79cc7f02cadf968b7a8fd2e5c44f78a
¨lukas¨ <lukystrikeibg@gmail.com>
2020-09-12 12:46:02 -0500

    agregue el parser home

--- funciones_wbs.py
+++ funciones_wbs.py
@@ -29,3 +29,22 @@ def parse_notice(link,today):
 
     except  ValueError as ve:
         print(ve)
+# cabecera   union
+def parse_home():
+    try:
+        response= requests.get(HOME_URL)
+        if response.status_code ==200:
+           home=response.content.decode('utf-8')
+           parsed=html.fromstring(home)
+           links_to_notices=parsed.xpath(XPATH_Link_TO_ARTICLE)
+           #print(links_to_notices)
+           today= datetime.date.today().strftime('%d-%m-%Y')
+           if not os.path.isdir(today):
+                os.mkdir(today)
+
+           for link in links_to_notices:
+                parse_notice(link, today)
+        else:
+            raise valueError(f'error:{response.status_code}')
+    except ValueError as ve:
+        print(ve)
